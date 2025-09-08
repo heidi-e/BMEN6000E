f017223a9dbb474a7a5674b62e5c39212b33a0cd
heidi-e <100874463+heidi-e@users.noreply.github.com>
2025-09-08 15:25:51 -0400

    update circuit

--- Lab_1/BMEN6000_thermistor_template/BMEN6000_thermistor_template.ino
+++ Lab_1/BMEN6000_thermistor_template/BMEN6000_thermistor_template.ino
@@ -9,14 +9,15 @@ BMEN 6000 - Signal Processing for Medical Devices 2025
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
+#include <math.h>
 
 // Input parameters
 
-float R1 = ;
-float R2 = ;
-float R3 = ;
-float inVoltage = ;
-float sampRate = ;
+float R1 = 10;
+float R2 = 10;
+float R3 = 10;
+float inVoltage = 3.3;
+float sampRate = 256;
 
 // DAQ variables
 
@@ -28,15 +29,15 @@ int V2;
 float Vdiff;
 float resistance;
 float logR;
-float SHa = ;
-float SHb = ;
-float SHc = ;
+float SHa = 1e-3;
+float SHb = 1e-5;
+float SHc = 1e-6;
 float temp;
 
 // Moving average variables
 
 int i;
-const int buffsize = ;
+const int buffsize = 1;
 float tempBuff[buffsize];
 float sumTemp = 0;
 float avgTemp;
@@ -53,24 +54,24 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  //Uncomment to use SD card
+//Uncomment to use SD card
 
-  // SD.begin(chipSelect);
-  // if (!SD.begin(chipSelect)) {
-  //   Serial.println("SD not found ");
-  //   return;
-  // }
-  // Serial.println("SD found");
+SD.begin(chipSelect);
+if (!SD.begin(chipSelect)) {
+  Serial.println("SD not found ");
+  return;
+}
+Serial.println("SD found");
 
-  // tempRecord = SD.open("tempLog.txt", FILE_WRITE);
-  // if (tempRecord){
-  // tempRecord.println("Voltage (1)  Voltage (2)  Voltage (diff)  Resistance  Temp (C)  Average temp (C)");
-  // tempRecord.close();
-  // }
-  // else {
-  //   Serial.println("error opening tempRecord.txt");
-  //   return;
-  // }
+tempRecord = SD.open("tempLog.txt", FILE_WRITE);
+if (tempRecord){
+tempRecord.println("Voltage (1)  Voltage (2)  Voltage (diff)  Resistance  Temp (C)  Average temp (C)");
+tempRecord.close();
+}
+else {
+  Serial.println("error opening tempRecord.txt");
+  return;
+}
 
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
@@ -87,20 +88,24 @@ void loop() {
   // Calculate difference between nodes (equivalent to voltage across bridge)
   // 3 pts
 
+  Vdiff = V1 - V2;
+
 
   // Calculate resistance from voltage
   // 7 pts
-
+  resistance = ((R2 * inVoltage - (R1 + R2) * Vdiff) / (R1 * inVoltage + (R1 + R2))* Vdiff) * R3;
 
 
   // Calculate temperature from resistance
   // 10 pts
 
+  temp = 1/(SHa + SHb * log(resistance) + SHc*(pow(log(resistance),3)));
 
 
   // Moving average
   // 10 pts
-
+  // select time window and calculate average of that time window
+  // buff size is the specific window size
 
 
   // Output (Serial.print prints to the serial monitor; tempRecord.print prints to the SD card)
@@ -119,30 +124,30 @@ void loop() {
 
   // Uncomment to use SD card
   
-  // tempRecord = SD.open("tempLog.txt", FILE_WRITE);
+  tempRecord = SD.open("tempLog.txt", FILE_WRITE);
   
-  // if (tempRecord) {
-  //   tempRecord.print(V1);
-  //   tempRecord.print("\t");
-  //   tempRecord.print(V2);
-  //   tempRecord.print("\t");
-  //   tempRecord.print(Vdiff);
-  //   tempRecord.print("\t");
-  //   tempRecord.print(resistance);
-  //   tempRecord.print("\t");
-  //   tempRecord.print(temp);
-  //   tempRecord.print("\t");
-  //   tempRecord.println(avgTemp);
-  //   tempRecord.close();
-  // }
-  // else {
-  //   Serial.println("error opening tempLog.txt");
-  //   return;
-  // }
+  if (tempRecord) {
+    tempRecord.print(V1);
+    tempRecord.print("\t");
+    tempRecord.print(V2);
+    tempRecord.print("\t");
+    tempRecord.print(Vdiff);
+    tempRecord.print("\t");
+    tempRecord.print(resistance);
+    tempRecord.print("\t");
+    tempRecord.print(temp);
+    tempRecord.print("\t");
+    tempRecord.println(avgTemp);
+    tempRecord.close();
+  }
+  else {
+    Serial.println("error opening tempLog.txt");
+    return;
+  }
   
   // Implement sampling rate: 5 pts
 
-  delay();  
+  delay(100);
 
 }
 
